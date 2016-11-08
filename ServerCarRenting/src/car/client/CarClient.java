248f05c4970962c46fee2dd50b65f2584af4d237
Sim <rene.sim@laposte.net>
2016-11-08 22:14:15 +0100

    Add toString

--- ServerCarRenting/src/car/client/CarClient.java
+++ ServerCarRenting/src/car/client/CarClient.java
@@ -1,7 +1,6 @@
 package car.client;
 
 import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
 import java.util.Date;
 
 import car.server.CarsService;
@@ -9,7 +8,9 @@ import car.server.CarsService;
 public class CarClient {
 	public static void main(String[] args) {
 		try {
-			String codebase = "file://./";
+			String codebase = "file://./car/server/";
+			String current = new java.io.File( "." ).getCanonicalPath();
+			System.out.println(current);
 			System.setProperty("java.rmi.server.codebase", codebase);
 			System.setProperty("java.security.policy", "grant.policy");
 			//System.setSecurityManager(new RMISecurityManager());
