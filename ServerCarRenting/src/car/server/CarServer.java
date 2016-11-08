f6364fc089e20e92b15bc63cb33b3ccbfa38ae97
Sim <rene.sim@laposte.net>
2016-11-08 20:01:53 +0100

    Server for the project

---
+++ ServerCarRenting/src/car/server/CarServer.java
@@ -0,0 +1,14 @@
+package car.server;
+
+import java.rmi.Naming;
+
+public class CarServer {
+	public static void main(String[] args) {
+		try {
+			CarsService service = new CarsServiceImpl();
+			Naming.rebind("rmi://localhost:1099/CarsService", service);
+		} catch (Exception e) {
+			System.out.println("Trouble: " + e);
+		}
+	}
+}
