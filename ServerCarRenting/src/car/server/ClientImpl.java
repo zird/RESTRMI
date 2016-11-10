62bd2bdfcff677ea35d4444ecf4f6a0e93d4fc1c
Christophe CHHEANG <cchheang@advanced-schema.com>
2016-11-10 22:53:23 +0100

    move ClientImpl client to server

---
+++ ServerCarRenting/src/car/server/ClientImpl.java
@@ -0,0 +1,53 @@
+package car.server;
+
+import java.util.Objects;
+
+public class ClientImpl implements Client {
+	private final String login;
+	private final Status status;
+	private final String password;
+	private final String firstname;
+	private final String lastname;
+
+	public ClientImpl(String login, String password, Status status, String firstname, String lastname) {
+		this.login = Objects.requireNonNull(login);
+		this.password = Objects.requireNonNull(password);
+		this.status = Objects.requireNonNull(status);
+		this.firstname = Objects.requireNonNull(firstname);
+		this.lastname = Objects.requireNonNull(lastname);
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPwd() {
+		return password;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "First name: " + firstname + "\nLast name: " + lastname + "\nLogin: " + login + "\nStatus: " + status;
+	};
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Client)) {
+			return false;
+		}
+		ClientImpl client = (ClientImpl) obj;
+		return login.equals(client.login) && password.equals(client.password);
+	}
+}
