b7c0fd12b7bd709d67622513778739aac0eca19f
Christophe CHHEANG <cchheang@advanced-schema.com>
2016-11-13 22:45:18 +0100

    Client : rename method getPwd to getPassword ; add method to add/remove/get of a client's basket

--- ServerAndClient/ClientCarRenting/src/Client.java
+++ ServerAndClient/ClientCarRenting/src/Client.java
@@ -1,10 +1,13 @@
 import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface Client extends Remote, Serializable {
 
 	public String getLogin() throws RemoteException;
+
+	public String getPassword() throws RemoteException;
 
 	public String getLastname() throws RemoteException;
 
@@ -15,4 +18,12 @@ public interface Client extends Remote, Serializable {
 	public void notifyRent(Car car) throws RemoteException;
 
 	public void notifyReturn(Car car) throws RemoteException;
+
+	public void addCarToBasket(Car car) throws RemoteException;
+
+	public void removeCarFromBasket(Car car) throws RemoteException;
+
+	public void removeAllFromBasket() throws RemoteException;
+
+	public List<Car> getBasket() throws RemoteException;
 }
