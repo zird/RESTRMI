f1d9510a0958beaba56a820ef5726bb5eb9b0327
bryght7 <leebryan@hotmail.fr>
2016-11-14 10:15:55 +0100

    Lancer CarClientGUI pour lancer le client avec interface graph

---
+++ ServerAndClient/ClientCarRenting/src/CarClientGUI.java
@@ -0,0 +1,16 @@
+import java.rmi.RMISecurityManager;
+
+@SuppressWarnings("deprecation")
+public class CarClientGUI {
+	public static void main(String[] args) throws Exception {
+		String codebase = "file:../../ServerCarRenting/bin/";
+		System.setProperty("java.rmi.server.codebase", codebase);
+		System.setProperty("java.security.policy", "grant.policy");
+		System.setSecurityManager(new RMISecurityManager());
+
+		GUI GUI = new GUI();
+		GUI.start(); // open authentication window
+
+	}
+
+}
